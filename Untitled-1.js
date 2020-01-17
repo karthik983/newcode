1139490dd112c619098cfe2522fab0deb3c1920b
karthik983 <karthikh032@gmail.com>
2020-01-17 15:12:44 +0530

    first commit

---
+++ Untitled-1.js
@@ -0,0 +1,78 @@
+//console.log(12)
+//Problem-1
+const num=(inp)=>{
+    if(inp%2===0){
+    return 'even'
+    }
+    return 'odd'
+    }
+    
+   /* Problem-2
+    const num=()=>{
+        for(let i=1;i<=100;i++){
+        if(i%3===0 && i%5===0){
+         console.log("FizzBuzz");
+         }
+         else if(i%3===0){
+         console.log( "Fizz");
+         }
+        else if(i%5===0){
+         console.log( "Buzz");
+         }
+        else{
+        console.log(i)
+        }
+        }
+        }
+
+        problem-3
+        const arr1=(arr)=>{
+            let a=0;
+            for(let i=0;i<arr.length;i++){
+            a+=arr[i]
+            
+            }
+            return a
+            }
+
+            problem-4
+            const num=(inp)=>{
+                if(inp>0){
+                return"positive"
+                }else{
+                return "negative"
+                }
+                }
+
+                problem-5
+                const arr=(arr1,arr2)=>{
+                    let c=0
+                    c=arr1
+                    arr1=arr2
+                    arr2=c
+                    console.log( arr1,arr2)
+                    }
+
+
+
+
+                    var arr = [1,2,3]
+                    ​//Declaring an array
+                    function copyArray(arr){//function declaration
+                    var output = []   //declaring an empty array
+                    for(var i=0; i<arr.length;i++){
+                          output.push(arr[i] * 2) 
+                        }
+                    ​
+                    return output 
+                    ​
+                    }
+                    ​
+                    var result = copyArray(arr)
+//declaring a variable result whose output is uninitialized and creates an execution context(along with local memory)
+//thread,memory,callstack(related to )
+                    
+                    //SQL uses a specific schema based on relational databases 
+                    
+                    
+                    */
